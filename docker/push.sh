29a029baaf329905c3be4ec96a3f0adbb0972015
Corentin Chéron <chronc@tcd.ie>
2018-01-30 13:24:52 +0000

    add docker push script

---
+++ docker/push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Base Script File (build.sh)
+# Created: Mon 29 Jan 2018 18:50:25 GMT
+# Version: 1.0
+#
+# This Bash script was developped by Cory.
+#
+# (c) Cory <sgryco@gmail.com>
+
+# tag image
+docker tag citynet asegroup11/all_servers:citynet
+
+# push image
+docker push asegroup11/all_servers:citynet
