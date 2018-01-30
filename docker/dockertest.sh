83946dc51856b2d08259d528ed437940cd2c6b3c
Corentin Chéron <chronc@tcd.ie>
2018-01-30 14:01:14 +0000

    more python deps

--- docker/dockertest.sh
+++ docker/dockertest.sh
@@ -6,7 +6,12 @@ trap error ERR
 
 
 
-python --version 
+python --version
 [ `python3 --version | cut -d' ' -f2` == "3.5.2" ]
 python -c "print('Hello World')"
-java -version
+[ `python -c "import django; print(django.__version__)"` == "1.11.6" ]
+[ `python -c "import celery; print(celery.__version__)"` == "4.1.0" ]
+[ "`python -c "import psycopg2; print(psycopg2.__version__)"`" == "2.7.3.2 (dt dec pq3 ext lo64)" ]
+[ "`python -c "import oauth2client; print(oauth2client.__version__)"`" == "4.1.2" ]
+[ "`python -c "import geojson; print(geojson.__version__)"`" == "2.3.0" ]
+#java -version
