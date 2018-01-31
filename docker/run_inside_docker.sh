0c0f51cf8287d1521873e942220f9714349397f3
Corentin Chéron <chronc@tcd.ie>
2018-01-31 13:20:35 +0000

    script to run inside docker

---
+++ docker/run_inside_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Base Script File (build.sh)
+# Created: Mon 29 Jan 2018 18:50:25 GMT
+# Version: 1.0
+#
+# This Bash script was developped by Cory.
+#
+# (c) Cory <sgryco@gmail.com>
+
+# --rm: remove image after run
+# --net host copy host network connection
+# -v bind host folder inside docker
+# -w workdir inside the docker
+
+
+docker run --net host --rm -v $PWD:/app -w /app asegroup11/all_servers:citynet "$@"
