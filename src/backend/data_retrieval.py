0a2926b4e6a34886124de8bdd6ea5fedc35fe0c6
Corentin Chéron <chronc@tcd.ie>
2018-02-01 16:53:13 +0000

    fix pep8

--- src/backend/data_retrieval.py
+++ src/backend/data_retrieval.py
@@ -4,8 +4,8 @@ dbikes_apikey = '3665c1ce997b7a7d4384ea241251b2f606647b20'
 
 
 def get_dbikes_stations():
-    url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey={}'.format(
-        dbikes_apikey)
+    url = ('https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey={}'
+           ''.format(dbikes_apikey))
     response = requests.get(url)
     dbikes_stations = response.json()
     return dbikes_stations
