c8cd9d0fa8774486dcd3736c446734360b2b8de7
Corentin Chéron <chronc@tcd.ie>
2018-02-01 20:20:27 +0000

    add scripts to run postgres inside docker

---
+++ docker/db_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Base Script File (docker/run_db.sh)
+# Created: Thu 01 Feb 2018 18:44:10 GMT
+# Version: 1.0
+#
+# This Bash script was developped by Cory.
+#
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+
+path="$(dirname $(realpath $0))"
+
+
+echo "creating volumes if absent"
+docker volume create dm1
+docker volume create db2
+docker volume create db3
+docker run --net host -P --rm -i -v db1:/etc/postgresql \
+  -v db2:/var/log/postgresql -v db3:/var/lib/postgresql \
+  -v "${path}/..":/app -w /app asegroup11/all_servers:citynet \
+  /bin/bash /app/docker/db_run_cmd
