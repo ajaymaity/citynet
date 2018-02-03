1f6c7e22ccf6f8ba865b795734ac5252224fcd1e
Corentin Cheron <chronc@tcd.ie>
2018-02-03 10:58:11 +0000

    fix django test, correct db and pep8

--- test/django_db/test_django_db/settings.py
+++ test/django_db/test_django_db/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'test_django_db.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+                    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                    'NAME': 'docker',
+                    'USER': 'docker',
+                    'PASSWORD': 'docker',
+                    'HOST': 'localhost',
+                    'PORT': '5432',
+                }
 }
 
 
@@ -86,16 +90,21 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
