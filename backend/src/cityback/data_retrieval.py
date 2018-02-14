15c3272ba430f0d01d7b946489b66a8bf716276e
Saksham Sinha <saksham290992@gmail.com>
2018-02-14 15:15:36 +0000

    added static and dynamic data retrieval and tests

--- backend/src/cityback/data_retrieval.py
+++ backend/src/cityback/data_retrieval.py
@@ -2,69 +2,85 @@
 
 import requests
 import logging
-# TODO move this API to a secure external configuration file
-dbikes_apikey = '3665c1ce997b7a7d4384ea241251b2f606647b20'
+
+
 
 __all__ = ["get_stations_list_from_contract", "get_apikey",
            "get_json_from_url", "get_contracts"]
 
-BIKES_URL = dict(
-    contracts="https://api.jcdecaux.com/vls/v1/contracts?apiKey={api_key:}",
-    station_info=("https://api.jcdecaux.com/vls/v1/stations/{"
-                  "station_number:}?"
-                  "contract={:contract_name}&apiKey={api_key:}"),
-    stations_list="https://api.jcdecaux.com/vls/v1/stations?apiKey={api_key:}",
-    stations_from_contract="https://api.jcdecaux.com/vls/v1/stations?contract"
-                           "={contract_name:}&apiKey={api_key:}",
-    static_stations_Dublin="https://developer.jcdecaux.com/rest/vls/stations"
-                           "/Dublin.json")
+class DataRetrieval:
+
+    def __init__(self):
+
+        # TODO move this API to a secure external configuration file
+        self.dbikes_apikey = '3665c1ce997b7a7d4384ea241251b2f606647b20'
+
+        self.BIKES_URL = dict(
+            contracts="https://api.jcdecaux.com/vls/v1/contracts?apiKey={api_key:}",
+            station_info=("https://api.jcdecaux.com/vls/v1/stations/{"
+                          "station_number:}?"
+                          "contract={:contract_name}&apiKey={api_key:}"),
+            stations_list="https://api.jcdecaux.com/vls/v1/stations?apiKey={api_key:}",
+            stations_from_contract="https://api.jcdecaux.com/vls/v1/stations?contract"
+                                   "={contract_name:}&apiKey={api_key:}",
+            static_stations_Dublin="https://developer.jcdecaux.com/rest/vls/stations"
+                                   "/Dublin.json")
+
+    def get_json_from_url(self, url):
+        """Get the json response from a url, log if error."""
+        response = requests.get(url)
+        if response.status_code != 200:
+            logging.error("Response {} from server when accessing url {}".format(
+                response.status_code, url))
+            return None
+        return response.json(encoding='utf-8')
 
 
-def get_json_from_url(url):
-    """Get the json response from a url, log if error."""
-    response = requests.get(url)
-    if response.status_code != 200:
-        logging.error("Response {} from server when accessing url {}".format(
-            response.status_code, url))
-        return None
-    return response.json(encoding='utf-8')
+    def get_contracts(self):
+        """Retrieve the list of contracts from the API."""
+        url = self.BIKES_URL["contracts"].format(api_key=self.dbikes_apikey)
+        return self.get_json_from_url(url)
 
 
-def get_contracts():
-    """Retrieve the list of contracts from the API."""
-    url = BIKES_URL["contracts"].format(api_key=dbikes_apikey)
-    return get_json_from_url(url)
+    def get_stations_list_from_contract(self, contract):
+        """Get the json of all the stations of a contract."""
+        url = self.BIKES_URL["stations_from_contract"].format(
+            contract_name=contract, api_key=self.dbikes_apikey)
+        stations = self.get_json_from_url(url)
+        return stations
 
 
-def get_stations_list_from_contract(contract):
-    """Get the json of all the stations of a contract."""
-    url = BIKES_URL["stations_from_contract"].format(
-        contract_name=contract, api_key=dbikes_apikey)
-    stations = get_json_from_url(url)
-    return stations
+    def check_connectivity(self):
+        """Verify that the key is working by listing the station information."""
+        contracts = self.get_contracts()
+        if contracts is None:
+            return False
+        contracts = [c for c in contracts if c['name'] == "Dublin"]
+        if len(contracts) == 0:
+            return False
 
+        stations = self.get_stations_list_from_contract(contracts[0]['name'])
+        if stations is None:
+            return False
+        return True
 
-def check_connectivity():
-    """Verify that the key is working by listing the station information."""
-    contracts = get_contracts()
-    if contracts is None:
-        return False
-    contracts = [c for c in contracts if c['name'] == "Dublin"]
-    if len(contracts) == 0:
-        return False
+    def get_static_data(self):
+        """Get static Dublin Bikes data."""
+        url = self.BIKES_URL["static_stations_Dublin"].format(api_key=self.dbikes_apikey)
+        return self.get_json_from_url(url)
 
-    stations = get_stations_list_from_contract(contracts[0]['name'])
-    if stations is None:
-        return False
-    return True
+    def get_dynamic_data(self):
+        """Get dynamic Dublin Bikes data."""
+        contracts = self.get_contracts()
+        contract = [c for c in contracts if c['name'] == "Dublin"]
+        stations = self.get_stations_list_from_contract(contract[0]['name'])
 
+        deleteList = ["position", "address", "name"]
+        for station in stations:
+            for d in deleteList:
+                del station[d]
+        return stations
 
-def get_apikey():
-    """Return the api access key."""
-    return dbikes_apikey
-
-
-# DEBUGGING
-if __name__ == '__main__':
-
-    print(check_connectivity())
+    def get_apikey(self):
+        """Return the api access key."""
+        return self.dbikes_apikey
--- backend/test/test_data_retrieval.py
+++ backend/test/test_data_retrieval.py
@@ -3,17 +3,35 @@
 """Test cases for the data retrieval part."""
 
 import unittest
-from cityback import data_retrieval
+from cityback.data_retrieval import DataRetrieval
 
 
-class TestAPI(unittest.TestCase):
+class TestDataRetrieval(unittest.TestCase):
     """Test the access key."""
 
-    def test_key(self):
+    def test_api(self):
         """Check the connectivity using the Dublin bikes key."""
-        self.assertTrue(data_retrieval.get_apikey().endswith("47b20"))
-        self.assertTrue(data_retrieval.check_connectivity())
+        dataRetrieval = DataRetrieval()
+        self.assertTrue(dataRetrieval.get_apikey().endswith("47b20"))
+        self.assertTrue(dataRetrieval.check_connectivity())
 
+    def test_static_retrieval(self):
+        """Check static data retrieval objects."""
+        dataRetrieval = DataRetrieval()
+        stationsInfo = dataRetrieval.get_static_data()
+        keysList = ["number", "longitude", "address", "latitude", "name"]
+        for stationInfo in stationsInfo:
+            keys = stationInfo.keys()
+            self.assertEqual(set(keys), set(keysList))
+
+    def test_dynamic_retrieval(self):
+        """Check dynamic data retrieval objects."""
+        dataRetrieval = DataRetrieval()
+        stationsData = dataRetrieval.get_dynamic_data()
+        keysList = ["number", "last_update", "status", "banking", "available_bikes", "contract_name", "bonus", "available_bike_stands", "bike_stands"]
+        for stationInfo in stationsData:
+            keys = stationInfo.keys()
+            self.assertEqual(set(keys), set(keysList))
 
 if __name__ == '__main__':
     unittest.main()
