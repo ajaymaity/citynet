bc5333b7edf569c7990b270369512d04d06d6787
Corentin Chéron <chronc@tcd.ie>
2018-02-16 09:27:32 +0000

    fix pep8 + pycache!!

--- backend/src/cityback/data_processing.py
+++ backend/src/cityback/data_processing.py
@@ -1,7 +1,10 @@
+"""TODO."""
 from celery import Celery
 
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
+
 @app.task
 def add(x, y):
-  return x + y
+    """TODO."""
+    return x + y
