86b75ccf293d8a9566b7c81338a87e1937f9955b
Corentin Chéron <chronc@tcd.ie>
2018-02-19 18:46:18 +0000

    add auto update and create db django

---
+++ cityback/create_db_admin.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Base Script File (test/django_db/test.sh)
+# Created: Fri 02 Feb 2018 15:38:55 GMT
+# Version: 1.0
+#
+# This Bash script was developped by Cory.
+#
+# (c) Cory <sgryco@gmail.com>
+
+echo "This will delete the sqlite database do you want to proceed? (yes)"
+read a
+if [ "$a" != "yes" ]; then
+  exit 0
+fi
+
+rm db.sqlite3
+python manage.py makemigrations
+python manage.py makemigrations storage
+python manage.py makemigrations scheduler
+python manage.py makemigrations retrieval
+python manage.py migrate
+
+echo "creating admin user: admin/adminadmin"
+echo "from django.contrib.auth.models import User; \
+  User.objects.filter(email='admin@example.com').delete();\
+  User.objects.create_superuser('admin', 'admin@example.com', 'adminadmin')" | \
+  python manage.py shell
+
+rabbitmq-server -detached
+celery -A cityback worker --beat --detach
+
+python manage.py runserver 0.0.0.0:8000
+
