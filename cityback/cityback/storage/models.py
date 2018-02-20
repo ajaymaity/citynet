c55581e3c4b62ee152c82e6b05ca01e0114cf7a3
Saksham Sinha <saksham290992@gmail.com>
2018-02-20 14:58:11 +0000

    added dynamic data retrieval

--- cityback/cityback/storage/models.py
+++ cityback/cityback/storage/models.py
@@ -12,9 +12,16 @@ class DublinBikesStation(models.Model):
     name = models.CharField(max_length=80)
     address = models.CharField(max_length=120)
     bonus = models.BooleanField(default=False)
+    contract_name = models.CharField(max_length=30)
+    banking = models.BooleanField(default=False)
 
-
-# class DublinBikesStationUpdate(models.Model):
-#    parent_station = models.ForeignKey(DublinBikesStation,
+# class DublinBikesStationRealTimeUpdate(models.Model):
+#     """A Dublin bikes station real time data."""
+#
+#     parent_station = models.ForeignKey(DublinBikesStation,
 #                                       on_delete=models.CASCADE)
-#    status = models.CharField(max_length=30)
+#     status = models.CharField(max_length=30)
+#     last_update = models.DateField()
+#     available_bikes = models.IntegerField()
+#     available_bike_stands = models.IntegerField()
+#     bike_stands = models.IntegerField()   
