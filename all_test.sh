85faa9bbf3eb604476ebb873afa857cef790cde6
Corentin Chéron <chronc@tcd.ie>
2018-02-20 16:54:21 +0000

    remove django test from all tests

--- all_test.sh
+++ all_test.sh
@@ -7,10 +7,9 @@ set -e
 PRE="docker/run_inside_docker.sh bash -c"
 if [ "$1" == "from_docker" ]; then
 	PRE="bash -c"
-else
-	docker/run_all_tests.sh
 fi
 $PRE "cd cityback && python setup.py test"
+$PRE "cd cityback && python manage.py test"
 $PRE "cd frontend && npm install && npm run test-ci"
 $PRE "cd node-realtime && npm install && npm run test-ci"
 
