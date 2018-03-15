468e2f589a40604fc504eb8085e93143652eca45
Corentin Chéron <chronc@tcd.ie>
2018-03-15 18:13:33 +0000

    add management code

--- cityback/cityback/storage/management/commands/createAdmin.py
+++ cityback/cityback/storage/management/commands/createAdmin.py
@@ -1,9 +1,13 @@
+"""Create a dev admin account."""
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    """Custom command."""
+
     def handle(self, *args, **options):
+        """Actual code."""
         User.objects.create_superuser(username='admin',
                                       password='admin',
                                       email='your@email.PK')
