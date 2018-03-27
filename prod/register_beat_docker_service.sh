6f90715abf33da232c1889efdf67ae7e1db2e5bb
Corentin Cheron <chronc@tcd.ie>
2018-03-28 00:41:40 +0100

    add beat start script

---
+++ prod/register_beat_docker_service.sh
@@ -0,0 +1,6 @@
+sudo docker run --hostname "cityback_beat" \
+  -v "/home/ubuntu/config_private_citynet/":/app/config_private/ \
+  -v /home/ubuntu/citynet:/app \
+  -d --restart unless-stopped \
+  asegroup11/all_servers:citynet \
+  /app/prod/start_prod_beat.sh
--- prod/start_prod_nginx.sh
+++ prod/start_prod_nginx.sh
@@ -3,20 +3,9 @@
 source /app/config_private/bash_import_secret_aws
 
 set -e
-mkdir -p /var/log/celery /var/log/redis
-service redis-server start
 
 python /app/cityback/manage.py collectstatic --noinput
 
-if ls /var/log/celery/*pid 1> /dev/null 2>&1; then
-  rm -f /var/log/celery/*pid
-fi
-
-cd /app/cityback
-celery multi start beat \
-      -A cityback --beat \
-      --pidfile="/var/log/celery/%n.pid" \
-      --logfile="/var/log/celery/%n.log"
 # start web servers
 nginx -g "daemon off;" > /var/log/nginx/access.log 2> /var/log/nginx/error.log
 
