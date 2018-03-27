e87ea5b38f52e822fcd7269b634cc4185dee69ba
Corentin Cheron <chronc@tcd.ie>
2018-03-28 00:33:08 +0100

    add redis for beat

--- prod/start_prod_nginx.sh
+++ prod/start_prod_nginx.sh
@@ -3,10 +3,15 @@
 source /app/config_private/bash_import_secret_aws
 
 set -e
+mkdir -p /var/log/celery /var/log/redis
+service redis-server start
 
 python /app/cityback/manage.py collectstatic --noinput
 
-mkdir -p /var/log/celery
+if ls /var/log/celery/*pid 1> /dev/null 2>&1; then
+  rm -f /var/log/celery/*pid
+fi
+
 cd /app/cityback
 celery multi start beat \
       -A cityback --beat \
