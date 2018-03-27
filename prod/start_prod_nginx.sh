4c11740c37ca58e57a1b59fabbaefd6108d8bd14
Corentin Chéron <chronc@tcd.ie>
2018-03-27 15:38:20 +0100

    fix stdout

--- prod/start_prod_nginx.sh
+++ prod/start_prod_nginx.sh
@@ -6,9 +6,7 @@ set -e
 
 python /app/cityback/manage.py collectstatic --noinput
 
-ln -sf /dev/stdout /var/log/nginx/access.log \
-	&& ln -sf /dev/stderr /var/log/nginx/error.log
 
 # start web servers
-nginx -g "daemon off;"
+nginx -g "daemon off;" > /var/log/nginx/access.log 2> /var/log/nginx/error.log
 
