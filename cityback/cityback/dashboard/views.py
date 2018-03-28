95bb1dc5a9877a369e1c487b5e3e7bee5ceb1653
Corentin Cheron <chronc@tcd.ie>
2018-03-28 11:24:29 +0100

    add instance hostname to rtdemo

--- cityback/cityback/dashboard/views.py
+++ cityback/cityback/dashboard/views.py
@@ -1,7 +1,10 @@
 """Dashboard views."""
 from django.shortcuts import render
+import os
 
 
 def rtDemo(request):
     """Serve the index."""
-    return render(request, "rtDemo.html", {})
+
+    return render(request, "rtDemo.html", {'hostname':
+                                           os.environ.get("HOSTNAME", "None")})
