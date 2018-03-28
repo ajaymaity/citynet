74884cf0fd244494026dafa14ba7c0eee6a2c5c2
Corentin Cheron <chronc@tcd.ie>
2018-03-28 11:14:46 +0100

    fix register daphne

--- prod/register_daphne.sh
+++ prod/register_daphne.sh
@@ -1,5 +1,5 @@
 sudo docker pull asegroup11/all_servers:citynet
-sudo docker run --hostname "cityback_daphne" -p 8000:8000 \
+sudo docker run --name daphne --hostname "cityback_daphne" -p 8000:8000 \
   -v "/home/ubuntu/config_private_citynet/":/app/config_private/ \
   -v /home/ubuntu/citynet:/app -d --restart unless-stopped \
   asegroup11/all_servers:citynet /app/prod/start_prod_no_beat.sh
