ccdc8cfd0c12fbabdc07f85f14325b10249faf63
Corentin Cheron <chronc@tcd.ie>
2018-03-29 08:54:46 +0100

    update nginx start script

--- prod/start_prod_nginx.sh
+++ prod/start_prod_nginx.sh
@@ -3,8 +3,11 @@
 source /app/config_private/bash_import_secret_aws
 
 set -e
-
+# copy all static files to /var/www/static (see django documentation)
 python /app/cityback/manage.py collectstatic --noinput
+
+# copy the nginx configuration file
+cp /app/cityback/config_private/etc/nginx-enalbed /etc/nginx/site-enabled/default
 
 # start web servers
 nginx -g "daemon off;" > /var/log/nginx/access.log 2> /var/log/nginx/error.log
